abbef29f5991d4a6aa9db32b209a8d45fe4286fd
YASHMITHA A <yashmithavagman@gmail.com>
2025-06-20 13:19:33 +0530

    updated

--- main.py
+++ main.py
@@ -2,9 +2,34 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
+class ResearchResponse(BaseModel):
+    topic : str
+    summary : str
+    sources : list[str]
+    tools_used:list[str]
+
 llm = ChatOpenAI(model="gpt-4o-mini")
-response = llm.invoke("What is the meaning of yashmitha?")
-print(response)
+parser = PydanticOutputParser(pydatic_object=ResearchResponse)
+
+prompt = ChatMessagePromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """you are a research assistant that will help generate a research paper.
+               Answer the user query and use necessary tools.
+               Wrap the output in this format and provide no other text\n{format_instructions}
+               """,
+               
+        ),
+        ("placeholder","{chat_history}"),
+        ("human","{query}"),
+        ("placeholder","{agent_scratchpad}"),
+
+    ]
+)
+
